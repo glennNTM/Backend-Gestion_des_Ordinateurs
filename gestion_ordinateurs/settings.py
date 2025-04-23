2af37ffd3836d020a9cf56571db1865f946ce7b7
glennNTM <glennntoutoume82gmail.com>
2025-04-23 02:08:03 +0100

    .

--- gestion_ordinateurs/settings.py
+++ gestion_ordinateurs/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pti1-3se&c59$_j^bz68drr%#^^+8$q+720@9u6g2x-&78^(ew
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-rest-api-deploy-latest.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend-gestion-des-ordinateurs.onrender.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
